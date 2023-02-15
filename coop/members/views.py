95b11c57d973bff939fab94e2764cb6cf7ab391c
kaydanny <68244710+kaydanny@users.noreply.github.com>
2023-02-15 13:56:14 +0300

    app initialization
    app initialization

---
+++ coop/members/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def members(request):
+    template = loader.get_template("myfirst.html")
+    return HttpResponse(template.render())
