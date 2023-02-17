8074027305d1063a914879984f176f83cbdeea5e
kaydanny <68244710+kaydanny@users.noreply.github.com>
2023-02-17 09:56:33 +0300

    template upload
    template upload

--- coop/members/urls.py
+++ coop/members/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
--- coop/members/views.py
+++ coop/members/views.py
@@ -4,5 +4,15 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-    template = loader.get_template("myfirst.html")
+#    template = loader.get_template("myfirst.html")
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
+
+def register(request):
+#    template = loader.get_template("myfirst.html")
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
+def login(request):
+#    template = loader.get_template("myfirst.html")
+    template = loader.get_template("index.html")
     return HttpResponse(template.render())
